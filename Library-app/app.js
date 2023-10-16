b445f589f746832047e9e2324831b8def0bb84ed
hsperez6 <hsperez@gmail.com>
2023-10-16 00:44:04 -0700

    Database connected

--- Library-app/app.js
+++ Library-app/app.js
@@ -4,6 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
+
+
+
+var { sequelize } = require('./models');
+
+
+(async () => {
+	try {
+		await sequelize.authenticate();
+		console.log('Connection to the database successful!');
+	} catch (error) {
+		console.error('Error connecting to the database: ', error);
+	}
+})(); 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- Library-app/routes/index.js
+++ Library-app/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { Book } = require('../models');
+
+
+
+
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      res.status(500).send(error);
+    }
+  }
+};
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  // res.render('index', { title: 'Express' });
+  const allBooks = await Book.findAll();
+  console.log( allBooks.map(singleBook => singleBook.toJSON()));
+}));
 
 module.exports = router;
