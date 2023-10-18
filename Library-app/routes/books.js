1365683a807d346703060c232a6bccc52eaa5b03
hsperez6 <hsperez@gmail.com>
2023-10-17 19:18:12 -0700

    10/17/23

--- Library-app/routes/books.js
+++ Library-app/routes/books.js
@@ -18,22 +18,109 @@ function asyncHandler(cb){
 
 /* GET Books */
 router.get('/', asyncHandler(async (req, res, next) => {
-  const allBooks = await Book.findAll();
+  const allBooks = await Book.findAll({ order: [['author', 'ASC']]});
   res.render('index', { title: 'Books', allBooks });
   // res.json( allBooks.map(singleBook => singleBook.toJSON()));
 }));
 
 
 /* GET books/new shows create new book form*/
-router.get('/', asyncHandler(async (req, res, next) => {
-
-}));
+router.get('./books/new', (req, res) => {
+  res.render('new-book', {});
+});
 
 
 /* POST books/new Posts a new book to the database */
 router.post('/', asyncHandler(async (req, res, next) => {
-
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/" + article.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") { // checking the error
+      article = await Article.build(req.body);
+      res.render("articles/new", { article, errors: error.errors, title: "New Article" })
+    } else {
+      throw error;
+    }  
 }));
+
+/* POST create article. 
+router.post('/', asyncHandler(async (req, res) => {
+  let article;
+  try {
+    article = await Article.create(req.body);
+    res.redirect("/articles/" + article.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") { // checking the error
+      article = await Article.build(req.body);
+      res.render("articles/new", { article, errors: error.errors, title: "New Article" })
+    } else {
+      throw error;
+    }  
+  };
+}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+/* GET articles listing.
+router.get('/', asyncHandler(async (req, res) => {
+  const articles = await Article.findAll({ order: [['createdAt', 'DESC']] });
+  res.render("articles/index", { articles, title: "Sequelize-It!" });
+}));
+
+/* Create a new article form. 
+router.get('/new', (req, res) => {
+  res.render("articles/new", { article: {}, title: "New Article" });
+});
+
+/* POST create article. 
+router.post('/', asyncHandler(async (req, res) => {
+  let article;
+  try {
+    article = await Article.create(req.body);
+    res.redirect("/articles/" + article.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") { // checking the error
+      article = await Article.build(req.body);
+      res.render("articles/new", { article, errors: error.errors, title: "New Article" })
+    } else {
+      throw error;
+    }  
+  };
+}));
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* GET books/:id shows book detail form */
 router.get('/', asyncHandler(async (req, res, next) => {
